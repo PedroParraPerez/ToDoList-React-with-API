c874cc7d974adfcf215028a7523227c0fa7bfae0
PedroParraPerez <pedroparra06091994@gmail.com>
2022-02-09 22:33:50 +0000

    need api

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const ToDoList = () => {
+	const [list, setList] = useState([]);
+	const [task, setTask] = useState();
+
+	const HandelSubmit = (e) => {
+		//Para cuando el formulario se envie
+
+		e.preventDefault();
+		setList([...list, task]);
+		setTask("");
+	};
+
+	const Delete = (index) => {
+		let tmp = list;
+		list.splice(index, 1);
+		setList([...tmp]);
+	};
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="wrap">
+				<h3>ToDoList</h3>
+
+				<form onSubmit={HandelSubmit}>
+					<label htmlFor="task"></label>
+					<br />
+					<input
+						className="input"
+						type="text"
+						name="task"
+						placeholder="Añadir tarea..."
+						value={task}
+						onChange={(e) => {
+							setTask(e.target.value);
+						}}
+					/>
+					<button type="submit">Añadir Tarea</button>
+				</form>
+				<div className="task_list">
+					<ul>
+						{list.map((task, i) => (
+							<li key={i}>
+								{task}
+								<span
+									onClick={() => Delete(i)}
+									className="delete">
+									🗑️
+								</span>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</>
 	);
 };
 
-export default Home;
+export default ToDoList;
